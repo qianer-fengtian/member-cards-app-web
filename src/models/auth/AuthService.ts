9a007cebdd3f875ea825049923fa83c57c4948a1
Qianer Fengtian <perplex0112358@icloud.com>
2020-01-25 00:29:23 +0900

    パスワードをハッシュ化して送るようにした（意味ある？）

--- src/models/auth/AuthService.ts
+++ src/models/auth/AuthService.ts
@@ -1,12 +1,14 @@
 import axios from '@/plugins/axios'
 import store from '@/store'
+import jsSHA from 'jssha'
 import { User } from '@/models/user/User'
 
 export default {
   async login(user: User) {
     store.commit('AUTH_REQUEST')
-    const res = await axios.post('/login', user)
-
+    const shaObj = new jsSHA('SHA-256', 'TEXT')
+    shaObj.update(user.password)
+    const res = await axios.post('/login', Object.assign(user, {password: shaObj.getHash('HEX')}))
     const token = res.data.token
     localStorage.setItem('user-token', token)
     store.commit('AUTH_SUCCESS', token)
