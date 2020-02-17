e27a16c5edf903419ea04a168b9081756fd68a8b
Qianer Fengtian <perplex0112358@icloud.com>
2020-02-18 00:08:02 +0900

    社員カードをすべて・部署別・チーム別に分けて表示できるようにした

--- src/views/cards/CardList.vue
+++ src/views/cards/CardList.vue
@@ -1,18 +1,30 @@
 <template>
   <v-container fluid>
+    <v-btn-toggle v-model="viewMode">
+      <v-btn :value="0">すべて</v-btn>
+      <v-btn :value="1">部署別</v-btn>
+      <v-btn :value="2">チーム別</v-btn>
+    </v-btn-toggle>
     <v-row>
-      <template v-for="member in members">
-        <v-col
-          :key="member.id"
-          sm="6"
-          md="4"
-          lg="3"
-        >
-          <MemberListItem
-            :member="member"
-            :loading="loading"
-          />
-        </v-col> 
+      <template v-for="[name, members] in cards">
+        <v-col :key="name" cols="12">
+          <span class="headline">
+            {{ name }}
+          </span>
+        </v-col>
+        <template v-for="member in members">
+          <v-col
+            :key="member.id"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <MemberListItem
+              :member="member"
+              :loading="loading"
+            />
+          </v-col> 
+        </template>
       </template>
     </v-row>
   </v-container>
@@ -22,7 +34,9 @@
 import {Component, Vue, Prop} from 'vue-property-decorator'
 import {Member} from '@/models/member/Member'
 import MemberService from '@/models/member/MemberService'
+import {Department} from '@/models/department/Department'
 import DepartmentService from '@/models/department/DepartmentService'
+import {Team} from '@/models/team/Team'
 import TeamService from '@/models/team/TeamService'
 
 @Component({
@@ -33,8 +47,24 @@ import TeamService from '@/models/team/TeamService'
 export default class MemberList extends Vue {
   private loading: boolean = false
   private members: Array<Member> = []
+  private departments: Array<Department> = []
+  private teams: Array<Team> = []
   private departmentNameMap: Map<string, string> = new Map()
   private teamNameMap: Map<string, string> = new Map()
+  private viewMode: number = 0
+
+  private get cards() {
+    const cards = [
+      new Map([['all', this.members]]),
+      new Map(this.departments.map(department => {
+        return [department.name, this.members.filter(member => member.departmentId === department.id)]
+      })),
+      new Map(this.teams.map(team => {
+        return [team.name, this.members.filter(member => member.teamId === team.id)]
+      }))      
+    ]
+    return cards[this.viewMode]
+  }
 
   async created() {
     this.members = Array(20).join(',').split(',').map(() => this.getDummyMember())
@@ -42,6 +72,8 @@ export default class MemberList extends Vue {
     try {
       this.loading = true
       this.members = await MemberService.search()
+      this.departments = await DepartmentService.search()
+      this.teams = await TeamService.search()
       this.departmentNameMap = await DepartmentService.getNameMap()
       this.teamNameMap = await TeamService.getNameMap()
       this.members.forEach(member => {
