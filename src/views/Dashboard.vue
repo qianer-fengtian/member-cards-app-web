9502ee57301d8ffebdfe6fcd3be4f121fe6da5b0
Qianer Fengtian <perplex0112358@icloud.com>
2020-03-14 16:33:09 +0900

    ダッシュボードをリファクタリング

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,26 +1,20 @@
 <template>
-  <v-container fluid>
+  <v-container 
+    class="dashboard"
+    fluid 
+  >
     <v-row>
       <v-col cols="12">
-        <MemberComposition
-          :total="statistics.memberStastics.total"
-          :male-total="statistics.memberStastics.maleTotal"
-          :female-total="statistics.memberStastics.femaleTotal"
-        />
+        <MemberComposition :statistics="statistics" />
       </v-col>
       <v-col cols="12">
         <template v-if="!loading">
-          <AgeComposition :population-by-age="statistics.memberStastics.populationByAge" />
+          <AgeComposition :statistics="statistics" />
         </template>
       </v-col>
       <v-col cols="12">
         <template v-if="!loading">
-          <JoinedLeftComposition
-            :new-graduates="statistics.memberStastics.newGraduates"
-            :mid-careers="statistics.memberStastics.midCareers"
-            :numbers-of-employments="statistics.numbersOfEmployments"
-            :numbers-of-retirements="statistics.numbersOfRetirements"
-          />
+          <JoinedLeftComposition :statistics="statistics" />
         </template>
       </v-col>
     </v-row>
@@ -29,9 +23,6 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
-import DepartmentService from '@/models/department/DepartmentService'
-import MemberService from '@/models/member/MemberService'
-import {MemberStatistics} from '@/models/member/Member'
 import { Statistics } from '@/models/statistics/Statistics'
 import StatisticsService from '@/models/statistics/StatisticsService'
 
@@ -44,18 +35,7 @@ import StatisticsService from '@/models/statistics/StatisticsService'
 })
 export default class Dashboard extends Vue {
   private loading: boolean = false
-  private statistics: Statistics = {
-    numbersOfEmployments: [],
-    numbersOfRetirements: [],  
-    memberStastics: {
-      total: 0,
-      maleTotal: 0,
-      femaleTotal: 0,
-      newGraduates: 0,
-      midCareers: 0,
-      populationByAge: {},
-    },
-  }
+  private statistics = StatisticsService.newInstance()
 
   async created() {
     try {
@@ -74,4 +54,7 @@ export default class Dashboard extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.dashboard {
+  background-color: #fafafa;
+}
 </style>
